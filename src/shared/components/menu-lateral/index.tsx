caf87bb30a02c3d55d9fdf9da54875c9eb2894b4
Andrey Airson <andreyairson@gmail.com>
2023-01-12 03:33:08 -0300

    refactor: added theme switching functionality and added contrast colors in dark theme

--- src/shared/components/menu-lateral/index.tsx
+++ src/shared/components/menu-lateral/index.tsx
@@ -1,8 +1,19 @@
-import { Avatar, Divider, Drawer, List, useMediaQuery, useTheme } from '@mui/material'
+import {
+  Avatar,
+  Divider,
+  Drawer,
+  Icon,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  useMediaQuery,
+  useTheme
+} from '@mui/material'
 import { Box } from '@mui/system'
 import { FC } from 'react'
 import { IMenuLateralProps } from 'shared/components/components-types'
-import { useDrawerContext } from 'shared/contexts'
+import { useAppThemeContext, useDrawerContext } from 'shared/contexts'
 import ListItemLink from './list-item-link'
 
 const MenuLateral: FC<IMenuLateralProps> = ({ children }) => {
@@ -10,6 +21,13 @@ const MenuLateral: FC<IMenuLateralProps> = ({ children }) => {
   const smDown = useMediaQuery(theme.breakpoints.down('sm'))
 
   const { isDrawerOpen, toggleDrawerOpen, drawerOptions } = useDrawerContext()
+
+  const { toggleTheme, themeName } = useAppThemeContext()
+
+  const handleClick = () => { 
+    toggleTheme()
+    console.log(themeName)
+  }
 
   return (
     <>
@@ -44,6 +62,16 @@ const MenuLateral: FC<IMenuLateralProps> = ({ children }) => {
                   onClick={smDown ? toggleDrawerOpen : undefined}
                 />
               ))}
+            </List>
+          </Box>
+          <Box>
+            <List component='nav'>
+              <ListItemButton onClick={handleClick}>
+                <ListItemIcon>
+                  <Icon>{themeName === 'light' ? 'dark_mode' : 'light_mode'}</Icon>
+                </ListItemIcon>
+                <ListItemText primary={themeName === 'light' ? 'Tema escuro' : 'Tema claro'} />
+              </ListItemButton>
             </List>
           </Box>
         </Box>
