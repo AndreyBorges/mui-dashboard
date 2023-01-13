9e671eadef747c1522403b245ab9465e81114a62
Andrey Airson <andreyairson@gmail.com>
2023-01-13 19:41:48 -0300

    feat: add  table in PealpeList

--- src/pages/people-listing/index.tsx
+++ src/pages/people-listing/index.tsx
@@ -1,24 +1,44 @@
-import { FC, useEffect, useMemo } from 'react'
+import { FC, useEffect, useMemo, useState } from 'react'
 import { BaseLayout } from 'shared/layouts'
 import { ListingTools } from 'shared/components'
 import { useSearchParams } from 'react-router-dom'
 import { PeopleSevices } from 'shared/services'
 import { useDebounce } from 'shared/hooks'
+import { IPeople } from 'shared/services/api/types'
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow
+} from '@mui/material'
 
 const PeopleListing: FC = () => {
   const [searchParams, setSearchParams] = useSearchParams()
   const { debounce } = useDebounce(3000)
+  const [rows, setRows] = useState<IPeople[]>([])
+  const [totalCount, setTotalCount] = useState(0)
+  const [isLoading, setIsLoading] = useState(true)
+
   const search = useMemo(() => {
     return searchParams.get('search') || ''
   }, [searchParams])
 
   useEffect(() => {
+    setIsLoading(true)
+
     debounce(() => {
       PeopleSevices.getAll(1, search).then(response => {
+        setIsLoading(false)
+
         if (response instanceof Error) {
           alert(response.message)
           return
         } else {
+          setRows(response.data)
+          setTotalCount(response.totalCount)
           console.log(response)
         }
       })
@@ -37,7 +57,34 @@ const PeopleListing: FC = () => {
         />
       }
     >
-      {}
+      <TableContainer
+        component={Paper}
+        variant='outlined'
+        sx={{
+          m: 1,
+          width: 'auto'
+        }}
+      >
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Ações</TableCell>
+              <TableCell>Nome Completo</TableCell>
+              <TableCell>Email</TableCell>
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+            {rows.map(row => (
+              <TableRow key={row.id}>
+                <TableCell>Ações</TableCell>
+                <TableCell>{row.completedName}</TableCell>
+                <TableCell>{row.email}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </BaseLayout>
   )
 }
